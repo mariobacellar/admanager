61e33bd4b3f1a65f7f09bad42271080a24f33008
Mario da Costa Bacellar <mariobacellar@users.noreply.github.com>
2021-05-20 19:09:40 -0300

    Update AdsController.java

--- src/main/java/br/com/mariobacellar/admanager/controller/AdsController.java
+++ src/main/java/br/com/mariobacellar/admanager/controller/AdsController.java
@@ -6,7 +6,9 @@ import br.com.mariobacellar.admanager.util.Util;
 
 import java.io.File;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
@@ -35,10 +37,22 @@ public class AdsController {
 		return    getJSON_Ads( );
 	}
 
+	@RequestMapping("/ads/{id}")
+	@ResponseBody
+	public String getAds(@PathVariable String id){
+		System.out.println("id=("+id+")");
+		return    getJSON_Ads(id);
+	}
 	
 	private String getJSON_Ads() {
+		return getJSON_Ads("");
+	}
+
+	private String getJSON_Ads(String id) {
 		try {
-			String		filename= "src/main/resources/Ads.json";
+			String		filename= "src/main/resources/Ads"+id+".json";
+			
+			System.out.println("filename=("+filename+")");
 			File		file	= new File(filename);
 			String[]	json	= Util.doReadLines(file);
 			String 		jsonStr = Util.convert2String(json); 
